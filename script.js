1a93184956031e8683ecbae356a01daf9880d5fb
phongnv307 <79550670+phongnv307@users.noreply.github.com>
2022-02-01 15:23:47 +0700

    if

---
+++ script.js
@@ -0,0 +1,11 @@
+localStorage.setItem('đanh','0');
+localStorage.setItem('bao','0');
+localStorage.setItem('huy','0');
+localStorage.setItem('hoang','0');
+localStorage.setItem('nam','0');
+localStorage.setItem('phong','0');
+localStorage.setItem('quang','0');
+localStorage.setItem('thinh','0');
+localStorage.setItem('vu','0');
+localStorage.setItem('quynh','0');
+
