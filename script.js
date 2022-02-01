dbbd38fec1a381a17c6314d38ba6e9f509977921
phongnv307 <79550670+phongnv307@users.noreply.github.com>
2022-02-02 01:00:30 +0700

    func

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
-localStorage.setItem('đanh','0');
-localStorage.setItem('bao','0');
-localStorage.setItem('huy','0');
-localStorage.setItem('hoang','0');
-localStorage.setItem('nam','0');
-localStorage.setItem('phong','0');
-localStorage.setItem('quang','0');
-localStorage.setItem('thinh','0');
-localStorage.setItem('vu','0');
-localStorage.setItem('quynh','0');
+let phong = localStorage.getItem('phong');
+if(phong === null){
+    localStorage.setItem('dAnh','0');
+    localStorage.setItem('Bao','0');
+    localStorage.setItem('Huy','0');
+    localStorage.setItem('Hoang','0');
+    localStorage.setItem('Nam','0');
+    localStorage.setItem('Phong','0');
+    localStorage.setItem('Quang','0');
+    localStorage.setItem('Thinh','0');
+    localStorage.setItem('Vu','0');
+    localStorage.setItem('Quynh','0');
+}
+
+let cais = localStorage.getItem('cais');
+if(cais === null){
+    localStorage.setItem('cais','Bao');
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById(localStorage.getItem('cais')).classList.add('cais');
+});
+
+function addCais(event){
+    let i;
+    console.log(event);
+    var players = document.querySelectorAll('.players div');
+    for (i = 0; i < players.length; i++){
+        players[i].className = players[i].className.replace("cais", "");
+    }
+    event.parentElement.className += "cais";
+    localStorage.setItem('cais',event.innerText);
+}
+
+let num = localStorage.getItem('num');
+if(num === null){
+    localStorage.setItem('num','5');
+}
+
+const playerBtn = document.querySelector('.player-btn');
+const playerInput = document.querySelector('.player-input');
+playerBtn.addEventListener('click', function(){
+    if(playerInput.value != '' ){
+        localStorage.setItem('num',playerInput.value);
+    }
+});
 
