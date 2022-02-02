81eb5aa66e5e2b1b36eef8d1f6d6e8025224c64d
phongnv307 <79550670+phongnv307@users.noreply.github.com>
2022-02-02 13:51:33 +0700

    done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let phong = localStorage.getItem('phong');
+let phong = localStorage.getItem('Phong');
 if(phong === null){
     localStorage.setItem('dAnh','0');
     localStorage.setItem('Bao','0');
@@ -16,14 +16,14 @@ let cais = localStorage.getItem('cais');
 if(cais === null){
     localStorage.setItem('cais','Bao');
 }
-
+document.addEventListener("DOMContentLoaded", getM);
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById(localStorage.getItem('cais')).classList.add('cais');
+    document.querySelector('.add').children[2].innerText = localStorage.getItem('num');
 });
 
 function addCais(event){
     let i;
-    console.log(event);
     var players = document.querySelectorAll('.players div');
     for (i = 0; i < players.length; i++){
         players[i].className = players[i].className.replace("cais", "");
@@ -41,7 +41,50 @@ const playerBtn = document.querySelector('.player-btn');
 const playerInput = document.querySelector('.player-input');
 playerBtn.addEventListener('click', function(){
     if(playerInput.value != '' ){
-        localStorage.setItem('num',playerInput.value);
+        localStorage.setItem('num', playerInput.value);
+        document.querySelector('.add').children[2].innerText = playerInput.value;
+        playerInput.value = '';
     }
 });
 
+
+const toDoList = document.querySelector('.players');
+toDoList.addEventListener('click', vnd);
+function vnd(event){
+    const item = event.target;
+    var numM = Number(localStorage.getItem('num'));
+    var playerM = localStorage.getItem(item.parentElement.children[0].innerText);
+    cais = localStorage.getItem('cais');
+    var caisM = localStorage.getItem(cais);
+    if(item.parentElement.children[0].innerText === cais){
+        return;
+    }
+
+    if(item.innerText === '-'){  
+        playerM = Number(playerM) - numM;
+        localStorage.setItem(item.parentElement.children[0].innerText, playerM);
+
+        caisM = Number(caisM) + numM;
+        localStorage.setItem(cais, caisM);
+    }
+
+    if(item.innerText === '+'){  
+        playerM = Number(playerM) + numM;
+        localStorage.setItem(item.parentElement.children[0].innerText, playerM);
+
+        caisM = Number(caisM) - numM;
+        localStorage.setItem(cais, caisM);
+    }
+
+    getM();
+
+}
+
+function getM(){
+    let i;
+    var players = document.querySelectorAll('.players div');
+    for (i = 0; i < players.length; i++){
+        var playerM = localStorage.getItem(players[i].children[0].innerText);
+        players[i].children[2].innerText = playerM;
+    }
+}
