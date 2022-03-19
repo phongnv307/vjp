5f480afff1e5f526b9b336cf5da645fb524f5039
phongnv307 <79550670+phongnv307@users.noreply.github.com>
2022-03-19 11:01:48 +0700

    add

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-let phong = localStorage.getItem('Phong');
-if(phong === null){
-    localStorage.setItem('dAnh','0');
-    localStorage.setItem('Bao','0');
-    localStorage.setItem('Huy','0');
-    localStorage.setItem('Hoang','0');
-    localStorage.setItem('Nam','0');
-    localStorage.setItem('Phong','0');
-    localStorage.setItem('Quang','0');
-    localStorage.setItem('Thinh','0');
-    localStorage.setItem('Vu','0');
-    localStorage.setItem('Quynh','0');
-}
-
-let cais = localStorage.getItem('cais');
-if(cais === null){
-    localStorage.setItem('cais','Bao');
-}
-document.addEventListener("DOMContentLoaded", getM);
 document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById(localStorage.getItem('cais')).classList.add('cais');
-    document.querySelector('.add').children[2].innerText = localStorage.getItem('num');
+
+    let playerName;
+        if(localStorage.getItem('playerName') === null) {
+            playerName = [];
+        }
+        else {
+            playerName = JSON.parse(localStorage.getItem('playerName'));
+            playerName.forEach(function(name){
+                const newPlayer = document.createElement("div");
+                newPlayer.id = name;
+                const newName = document.createElement('span');
+                newName.classList.add('name');
+                newName.setAttribute('onclick','addCais(this)');
+                newName.innerHTML = name;
+                const minus = document.createElement('button');
+                minus.innerHTML = "-";
+                const money = document.createElement('span');
+                money.classList.add('money');
+                const plus = document.createElement('button');
+                plus.innerHTML = "+";
+                
+                newPlayer.appendChild(newName);
+                newPlayer.appendChild(minus);
+                newPlayer.appendChild(money);
+                newPlayer.appendChild(plus);
+                playerList.appendChild(newPlayer);
+            });
+            document.getElementById(localStorage.getItem('cais')).classList.add('cais');
+        }
+        
+        document.querySelector('.add').children[2].innerText = localStorage.getItem('num');
+
 });
+document.addEventListener("DOMContentLoaded", getM);
 
 function addCais(event){
     let i;
@@ -37,24 +49,24 @@ if(num === null){
     localStorage.setItem('num','5');
 }
 
-const playerBtn = document.querySelector('.player-btn');
-const playerInput = document.querySelector('.player-input');
-playerBtn.addEventListener('click', function(){
-    if(playerInput.value != '' ){
-        localStorage.setItem('num', playerInput.value);
-        document.querySelector('.add').children[2].innerText = playerInput.value;
-        playerInput.value = '';
+const numBtn = document.querySelector('.num-btn');
+const numInput = document.querySelector('.num-input');
+numBtn.addEventListener('click', function(){
+    if(numInput.value != '' ){
+        localStorage.setItem('num', numInput.value);
+        document.querySelector('.add').children[2].innerText = numInput.value;
+        numInput.value = '';
     }
 });
 
 
-const toDoList = document.querySelector('.players');
-toDoList.addEventListener('click', vnd);
+const playerList = document.querySelector('.players');
+playerList.addEventListener('click', vnd);
 function vnd(event){
     const item = event.target;
     var numM = Number(localStorage.getItem('num'));
     var playerM = localStorage.getItem(item.parentElement.children[0].innerText);
-    cais = localStorage.getItem('cais');
+    let cais = localStorage.getItem('cais');
     var caisM = localStorage.getItem(cais);
     if(item.parentElement.children[0].innerText === cais){
         return;
@@ -88,3 +100,50 @@ function getM(){
         players[i].children[2].innerText = playerM;
     }
 }
+
+const playerBtn = document.querySelector('.player-btn');
+const playerInput = document.querySelector('.player-input');
+playerBtn.addEventListener('click', function(){
+    
+    if(playerInput.value != '' ){
+        const newPlayer = document.createElement("div");
+        newPlayer.id = playerInput.value;
+        const newName = document.createElement('span');
+        newName.classList.add('name');
+        newName.setAttribute('onclick','addCais(this)');
+        newName.innerHTML = playerInput.value;
+        const minus = document.createElement('button');
+        minus.innerHTML = "-";
+        const money = document.createElement('span');
+        money.classList.add('money');
+        const plus = document.createElement('button');
+        plus.innerHTML = "+";
+        
+        newPlayer.appendChild(newName);
+        newPlayer.appendChild(minus);
+        newPlayer.appendChild(money);
+        newPlayer.appendChild(plus);
+        playerList.appendChild(newPlayer);
+
+        localStorage.setItem(playerInput.value,'0');
+        let cais = localStorage.getItem('cais');
+        if(cais === null){
+            localStorage.setItem('cais',playerInput.value);
+            newPlayer.classList.add('cais');
+        }
+
+        let playerName;
+        if(localStorage.getItem('playerName') === null) {
+            playerName = [];
+        }
+        else {
+            playerName = JSON.parse(localStorage.getItem('playerName'));
+        }
+
+        playerName.push(playerInput.value);
+        localStorage.setItem('playerName', JSON.stringify(playerName));
+        playerInput.value = '';
+        getM();
+        
+    }
+});
